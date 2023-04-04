c93ddb93634ee56ebfa98ab15aecef7903aadfcd
rongpi-kun <rongpi.kun14@gmail.com>
2023-04-04 12:23:01 +0530

    post request

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'data': {'name': 'Kutta Kalia'}}
+@app.get('/blog/{id}')
+def index(id: int):
+    return {'id': id,
+        'data': {'name': 'Kutta Kalia'}
+        }
 
 @app.get('/about')
 def about():
     return {'msg': 'about page'}
+
+@app.post('/blog')
+def create_blog():
+    return {
+        'msg': 'blog created'
+    }
